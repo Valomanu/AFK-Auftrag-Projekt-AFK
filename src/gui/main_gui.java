ee2740283ad27c9531a9f252d9d475e8237b7ae5
Sebastian Welser <welsed18@htl-kaindorf.at>
2021-10-14 15:51:34 +0200

    secondnt

--- src/gui/main_gui.java
+++ src/gui/main_gui.java
@@ -1,4 +1,32 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class main_gui {
+    private JButton button1;
+    private JPanel panelmain;
+
+
+    public main_gui(){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AFK");
+
+        frame.setContentPane(new main_gui().panelmain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width, screenSize.height);
+        frame.pack();
+        frame.setVisible(true);
+
+
+        System.out.println("Test");
+    }
+
+
 }
